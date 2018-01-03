92ca3ec92df864ae5e5a193295dc11335f8e1d43
secknv <secknv@gmail.com>
2018-01-03 22:59:13 +0000

    basic threading + controller input setup

--- src/main/java/Afmcc.java
+++ src/main/java/Afmcc.java
@@ -1,6 +1,25 @@
+
 public class Afmcc {
 
+    private int controllerIndex = 0;
+
+    public Afmcc() {
+        Cstate cstate = new Cstate(controllerIndex);
+        Thread cinput = new Thread(cstate, "cinput");
+        cinput.start();
+
+        //program aberto a receber memes do gui
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {
+            System.out.println("interrupt");
+        }
+
+        //eventualmente ordem do gui para fechar
+        cstate.close();
+    }
+
     public static void main(String[] args) {
-        System.out.println("run");
+        new Afmcc();
     }
 }
--- src/main/java/Cstate.java
+++ src/main/java/Cstate.java
@@ -1,3 +1,41 @@
-public class Cstate {
+import com.studiohartman.jamepad.ControllerManager;
+import com.studiohartman.jamepad.ControllerState;
 
+public class Cstate implements Runnable{
+
+    private boolean inloop = true;
+    private ControllerManager controllers;
+    private int cindex;
+
+    public Cstate(int x) {
+        controllers = new ControllerManager();
+        controllers.initSDLGamepad();
+        cindex = x;
+    }
+
+
+    @Override
+    public void run() {
+        while(inloop){
+            ControllerState currState = controllers.getState(cindex);
+
+            if(!currState.isConnected) {
+                System.out.println("controller not connected");
+                close();
+            }
+        }
+    }
+
+    public void close() {
+
+        //se ja esta fechado n fecha outra vez
+        if(inloop) {
+            inloop = false;
+            controllers.quitSDLGamepad();
+            System.out.println("close drivers");
+        }
+        else {
+            System.out.println("already closed");
+        }
+    }
 }
