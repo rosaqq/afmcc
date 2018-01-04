daa5b0a7d1f7d02b3dd928e527798742b3c9bcc9
secknv <secknv@gmail.com>
2018-01-04 19:01:25 +0000

    jna interface with hardware dlls

--- src/main/java/Afmcc.java
+++ src/main/java/Afmcc.java
@@ -1,9 +1,11 @@
+import com.sun.jna.ptr.IntByReference;
 
 public class Afmcc {
 
     private int controllerIndex = 0;
 
     public Afmcc() {
+        /*
         Cstate cstate = new Cstate(controllerIndex);
         Thread cinput = new Thread(cstate, "cinput");
         cinput.start();
@@ -17,6 +19,15 @@ public class Afmcc {
 
         //eventualmente ordem do gui para fechar
         cstate.close();
+        */
+
+        CU30Wrap culib = CU30Wrap.INSTANCE;
+
+        culib.CU30WrapperInit();
+        String ret = culib.CU30WOpen(new IntByReference(1), new IntByReference(1), new IntByReference(1), new IntByReference(1));
+        System.out.println(ret);
+        culib.CU30WrapperDispose();
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/CU30Wrap.java
@@ -0,0 +1,26 @@
+import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.win32.StdCallLibrary;
+
+public interface CU30Wrap extends StdCallLibrary {
+
+    CU30Wrap INSTANCE = (CU30Wrap) Native.loadLibrary("CU30Wrap", CU30Wrap.class);
+    CU30Wrap SYNC_INSTANCE = (CU30Wrap)Native.synchronizedLibrary(INSTANCE);
+
+    void CU30WrapperInit();
+    void CU30WrapperDispose();
+
+    String CU30WOpen(IntByReference USBVersion, IntByReference USBInstance, IntByReference DevID, IntByReference EEID);
+
+    int CU30WGetEEpromInfo(int USBInstance, int USBVersion, int DevID, int EEID,
+                           IntByReference pUSBVendorID, IntByReference pUSBProductID, IntByReference pUSBDeviceID,
+                           IntByReference pDeviceID, IntByReference pEEPromID, IntByReference pVersion,
+                           IntByReference pSerialNumber, IntByReference pCustomerID,
+                           String pCompany, String pDate, String pProductStr, String pCustomer, String pCustomerStr);
+
+    void CU30WClose(int USBInstance, int USBVersion, int DevID, int EEID);
+    void CU30WStop(int USBInstance, int USBVersion, int DevID, int EEID);
+    void CU30WStep(int USBInstance, int USBVersion, int DevID, int EEID, int Axis, int n, int Vel);
+    void CU30WSweep(int USBInstance, int USBVersion, int DevID, int EEID, int Vel, int Axis, int Timeout);
+
+}
